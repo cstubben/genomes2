6db63040eed214f9de088f7185f536b07a8beaa2
Chris Stubben <chris.stubben@gmail.com>
2013-05-25 13:32:16 -0600

    update esearch

--- R/esearch.R
+++ R/esearch.R
@@ -14,43 +14,42 @@ esearch <-function(term, db="pubmed", usehistory="y", parse=TRUE, verbose=TRUE, 
    
    if(showURL) print(search)
 
-   gp <-xmlParse( readLines(search) )
+   gp <- xmlParse( readLines(search) )
    # parse the output
    if(parse){
       # xvalue in genomes package 
       if( !is.na( xvalue(gp, "//ERROR") )){
-         xvalue(gp, "//ERROR")
+         gp <- xvalue(gp, "//ERROR")
       }else{
          count <- as.numeric(xvalue(gp, "//Count"))
          if(count == 0){  
-            stop( "No results found.")
+            if(verbose) print( "No results found.")
+            gp <- NULL
             # xvalue(gp, "//OutputMessage")  #OR
-         }
-         # return history object for esummary or efetch
-         if(usehistory == "y"){
-           if(verbose){ 
-              if(count == 1){ print("1 result found") 
-              }else{         print(paste( count, "results found")) }
-           }
-             query <- xvalue(gp, "//QueryKey")
-             web   <- xvalue(gp, "//WebEnv")
-             y <- data.frame( db= db, results=count,  query_key = query, WebEnv = web, stringsAsFactors=FALSE)
-             class(y) <- c("EntrezHistory", "data.frame")
-             y
-          # or id list
          }else{
-             retmax <- xvalue(gp, "//RetMax")
-             if(verbose){
-               if(retmax == count){
-                 if(count >1 ) print(paste( retmax, db, "ids returned"))
-               }else{          print(paste( retmax, " ", db, " ids returned (", count, " total ids)", sep="")) }
-             }
-             # paste(xpathSApply(gp, "//Id", xmlValue), collapse=",")  # comma-separated?
-             as.numeric( xpathSApply(gp, "//Id", xmlValue) )
-         } 
+            # return history object for esummary or efetch
+            if(usehistory == "y"){
+               if(verbose){ 
+                  if(count == 1){ print("1 result found") 
+                  }else{         print(paste( count, "results found")) }
+               }
+               query <- xvalue(gp, "//QueryKey")
+               web   <- xvalue(gp, "//WebEnv")
+               gp <- data.frame( db= db, results=count,  query_key = query, WebEnv = web, stringsAsFactors=FALSE)
+               class(gp) <- c("EntrezHistory", "data.frame")
+            # or id list
+            }else{
+               retmax <- xvalue(gp, "//RetMax")
+               if(verbose){
+                  if(retmax == count){
+                     if(count >1 ) print(paste( retmax, db, "ids returned"))
+                     }else{         print(paste( retmax, " ", db, " ids returned (", count, " total ids)", sep="")) }
+                  }
+               # paste(xpathSApply(gp, "//Id", xmlValue), collapse=",")  # comma-separated?
+               gp <- as.numeric( xpathSApply(gp, "//Id", xmlValue) )
+            } 
+         }
       }
-   # or return XML
-   }else{
-      gp
-   } 
+   }
+   gp 
 }
